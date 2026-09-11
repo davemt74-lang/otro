041a4dd7e27368f1c8038d8f0aa0ce60f5b15b66
davemt74-lang <davemt74@gmail.com>
2026-09-11 09:29:51 -0700

    Register v0.58 workflow automation feature schema

--- app/database.py
+++ app/database.py
@@ -17,6 +17,7 @@ FEATURE_SCHEMA_PATHS = (
     ROOT_DIR / "database" / "agent_routing.sql",
     ROOT_DIR / "database" / "agent_delegation_workflows.sql",
     ROOT_DIR / "database" / "agent_workflow_supervision.sql",
+    ROOT_DIR / "database" / "agent_workflow_automation.sql",
 )
 MIGRATION_PATTERN = re.compile(r"^(?P<version>\d{3})_.+\.sql$")
 SQLITE_BUSY_TIMEOUT_SECONDS = 30
